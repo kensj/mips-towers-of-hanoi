1b4b223cab12e6ba52757208961fcc02cd29b606
kensj <kenji.japra@gmail.com>
2018-06-15 16:32:50 -0400

    Initial data

---
+++ towers.asm
@@ -0,0 +1,9 @@
+.data
+Start:	.asciiz	"\nStarting number of disks: "
+AB:	.asciiz	"Transfer disk from A to B\n"
+AC:	.asciiz	"Transfer disk from A to C\n"
+BA:	.asciiz	"Transfer from disk B to A\n"
+BC:	.asciiz	"Transfer disk from B to C\n"
+CA:	.asciiz	"Transfer from disk C to A\n"
+CB:	.asciiz	"Transfer from disk C to B\n"
+Done:	.asciiz	"Done\n"
