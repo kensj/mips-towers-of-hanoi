72fc633a79d860ce6ccc06ebd717063276f8666f
kensj <kenji.japra@gmail.com>
2018-06-15 17:03:28 -0400

    formatting

--- towers.asm
+++ towers.asm
@@ -1,23 +1,19 @@
 .data
-	Start:	.asciiz	"\nStarting number of disks: "
-	AB:	.asciiz	"Transfer disk from A to B\n"
-	AC:	.asciiz	"Transfer disk from A to C\n"
-	BA:	.asciiz	"Transfer from disk B to A\n"
-	BC:	.asciiz	"Transfer disk from B to C\n"
-	CA:	.asciiz	"Transfer from disk C to A\n"
-	CB:	.asciiz	"Transfer from disk C to B\n"
-	Done:	.asciiz	"Done\n"
+	start:	.asciiz	"\nStarting Number of Disks: "
+	trans:	.asciiz	"\nMove Disk From "
+	to:	.asciiz	"to"
+	done:	.asciiz	"Done\n"
 	
 .text
 main:
 	li  $v0, 4	# System call for print string
-	la  $a0, Start	# Ask for the number of disks
+	la  $a0, start	# Ask for the number of disks
 	syscall
 	
 	li $v0, 5	# System call for read integer
     	syscall
-    	move $t0, $v0	# Move result to temporary register
     	
-    	li $v0, 10	# System call for exit program
+end:
+	li $v0, 10	# System call for exit program
     	syscall
-    	
+	
